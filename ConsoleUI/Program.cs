b5b548c1e21b508d110f056bc2bd44736332863f
Selman Jir CAN <selman_jir_85@hotmail.com>
2021-07-12 17:36:10 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,34 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ProductManagerTest();
+            //CategoryManagerTest();
+        }
+
+        private static void CategoryManagerTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll().Data)
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductManagerTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetAll().Data)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+    }
+}
